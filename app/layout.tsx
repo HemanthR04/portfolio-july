a5c1f938b4a5188a9551b374df3395a81c0e1695
HemanthR04 <Hemanth04051998@gmail.com>
2024-07-08 18:26:43 +0530

    UI fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap" rel="stylesheet"></link>
       </head>
       <body className={inter.className}>
-      <div className="absolute top-0 -z-10 h-full w-full bg-primary"><div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(168,109,244,0.5)] opacity-50 blur-[80px]"></div></div>
-        <NavigationBar/>
+        <div className="absolute top-0 -z-10 h-full w-full bg-primary"><div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(168,109,244,0.5)] opacity-50 blur-[80px]"></div></div>
+        <NavigationBar />
         {children}</body>
     </html>
   );
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -38,8 +38,8 @@ const ProjectCard = () => {
                         key={index} className='w-full md:min-w-[780px]  h-[260px] md:h-[440px] rounded-xl bg-secondary flex flex-col justify-end my-2'>
                         <motion.div 
                         whileHover={{ x: 20}}
-                        className='py-2 my-2 text-primary'>
-                            <h1 className=' font-sans font-bold text-xl md:text-5xl md:px-4 '>{project.title}</h1>
+                        className='py-2 my-2 text-primary '>
+                            <h1 className=' font-sans font-bold text-xl md:text-5xl md:px-4 px-4 '>{project.title}</h1>
                             
                         </motion.div>
                     </motion.div>
