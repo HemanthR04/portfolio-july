cbf50bfc19818b3d04a0bfb0bcb7911a9274a82d
HemanthR04 <Hemanth04051998@gmail.com>
2024-07-08 19:15:26 +0530

    Margin fix

--- components/RecentWork.tsx
+++ components/RecentWork.tsx
@@ -4,7 +4,7 @@ import ProjectCard from './ProjectCard'
 const RecentWork = () => {
     return (
         <div className='w-full  bg-primary flex flex-col items-center justify-start '>
-            <div className='my-32 md:my-12 text-center'>
+            <div className='my-24 md:my-12 text-center'>
                 <h1 className=' font-sans font-bold text-5xl md:text-7xl tracking-[-3px]'>RECENT</h1>
                 <h1 className=' text-5xl md:text-7xl font-serif font-semi '><span className='italic '> W</span>ORK</h1>
             </div>
